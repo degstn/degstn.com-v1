7966787eb953cc01c560a437020649fb78d46363
drew goldstein <drew@degstn.com>
2024-01-08 13:17:00 -0500

    updated placeholder page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from 'next/image'
+import Link from 'next/link'
+import './page.css'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className="text-5xl tracking-tight max-w-3xl font-semibold mb-4 mt-10">
-        temp
+    <main className="bg-white flex min-h-screen flex-col items-center justify-center">
+      <div className="relative flex place-items-center before:absolute ">
+        <h1 className="text-2xl text-gray-400 tracking-tight font-semibold ">
+        <Link href="github.com/degstn">
+          <p className="hover:text-gray-500">Drew Goldstein (Github)</p>
+        </Link>
         </h1>
       </div>
     </main>
