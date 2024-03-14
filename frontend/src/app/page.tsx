dbc3a6036d2064178706c4b141faf677a3441dcd
drew goldstein <drew@degstn.com>
2024-03-14 09:18:45 -0400

    new link

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,44 +8,51 @@ export default function Home() {
       <div className='lg:pt-64 pt-96'>
           <div className="grid grid-cols-10 lg:ml-10 md:pl-40 lg:px-32 sm:px-0 gap-5 sm:mt-20 mb-32 ">
           <div className="col-start-1 col-end-3 lg:px-20 md:p1-20 pl-10 ">
-          <span className="text-4xl bg-clip-text text-transparent bg-gradient-to-r from-rose-400 to-orange-300 tracking-tight font-normal">2024</span>
+          <span className="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-rose-400 to-orange-300 tracking-tight font-normal">2024</span>
           </div>
           <div className="col-end-7 col-span-2 lg:px-20 ">
-            <span className="text-4xl text-gray-400 tracking-tight font-normal">
+            <span className="text-3xl text-gray-400 tracking-tight font-normal">
             <Link href="https://github.com/degstn/">
                     <p className="hover:underline underline-offset-4">Inertia</p>
                   </Link>
             </span>
           </div>
+          <div className="col-end-7 col-span-2 lg:px-20 ">
+            <span className="text-3xl text-gray-400 tracking-tight font-normal">
+            <Link href="https://spatialwaypoints.com/">
+                    <p className="hover:underline underline-offset-4">Limen</p>
+                  </Link>
+            </span>
+          </div>
           <div className="col-end-7 col-span-2 lg:px-20 pb-10 ">
-            <span className="text-4xl text-gray-400 tracking-tight font-normal">
+            <span className="text-3xl text-gray-400 tracking-tight font-normal">
               <Link href="https://www.stelio.ai/">
                     <p className="hover:underline underline-offset-4">Stelio</p>
                   </Link>
             </span>
           </div>
-          <div className="col-start-1 col-end-3 lg:px-20 pl-10 ">
-          <span className="text-4xl text-gray-400 tracking-tight font-normal">2023</span>
+          <div className="col-start-1 col-end-3 lg:px-20 pb-10 ">
+          <span className="text-3xl text-gray-400 tracking-tight font-normal">2023</span>
           </div>
           <div className="col-end-7 col-span-2 lg:px-20 ">
-            <span className="text-4xl text-gray-400 tracking-tight font-normal">
+            <span className="text-3xl text-gray-400 tracking-tight font-normal">
             <Link href="https://docs.google.com/presentation/d/1HR3efx1HpUI6DvxthVhOzCj9JLPVE0YNZ744-EnUHAc/edit?usp=sharing">
                     <p className="hover:underline underline-offset-4">Service Seeker</p>
                   </Link>
             </span>
           </div>
           <div className="col-end-7 col-span-2 lg:px-20 pb-10 ">
-            <span className="text-4xl text-gray-400 tracking-tight font-normal">
+            <span className="text-3xl text-gray-400 tracking-tight font-normal">
             <Link href="https://aimazing.shop/">
                     <p className="hover:underline underline-offset-4">AImazing</p>
                   </Link>
             </span>
           </div>
-          <div className="col-start-1 col-end-3 lg:px-20 pl-10 ">
-          <span className="text-4xl text-gray-400 tracking-tight font-normal">2022</span>
+          <div className="col-start-1 col-end-3 lg:px-20 ">
+          <span className="text-3xl text-gray-400 tracking-tight font-normal">2022</span>
           </div>
           <div className="col-end-7 col-span-2 lg:px-20 ">
-            <span className="text-4xl text-gray-400 tracking-tight font-normal">
+            <span className="text-3xl text-gray-400 tracking-tight font-normal">
             <Link href="https://github.com/degstn/">
                     <p className="hover:underline underline-offset-4">redacted</p>
               </Link>
