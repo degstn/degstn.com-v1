530c596fe0e0d6dfece2f46b8845bf5af6f53a39
drew goldstein <drew@degstn.com>
2024-12-17 09:54:46 -0500

    added some needed insight to what this is

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,9 +10,18 @@ export default function Home() {
       {/* <div className="relative flex place-items-center before:absolute "> */}
       <meta name="theme-color" content="#fefdf0 dark:##1a1a1a"></meta>
       <div className={berkeleymono.className}>
-      <div className='lg:pt-64 pt-96 '>
+      <div className='lg:pt-24 pt-56 '>
           <div className="grid grid-cols-10 lg:ml-10 md:pl-40 lg:px-32 sm:px-0 gap-2 sm:mt-20 mb-16 ">
-          
+          <div className=" col-end-3 lg:px-20  ">
+          <span className="text-xl text-gray-600 tracking-tight font-light dark:text-gray-50">drew</span>
+          </div>
+          <div className=" col-end-3 lg:px-20 pb-36 ">
+          <span className="text-xl text-gray-400 tracking-tight font-normal dark:text-gray-50 ">
+            <Link href={"/cv.pdf"} scroll={false} onClick={(e) => { e.preventDefault()  }}>
+                    <p className="hover:underline underline-offset-4">cv</p>
+                  </Link>
+            </span>
+          </div>
           <div className=" col-end-3 lg:px-20  ">
           <span className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-rose-600 to-orange-300 tracking-tight font-light">2024</span>
           </div>
@@ -37,7 +46,7 @@ export default function Home() {
                   </Link>
             </span>
           </div>
-          <div className="col-end-7 col-span-2 lg:px-20 pb-10 ">
+          <div className="col-end-7 col-span-2 lg:px-20 pb-7 ">
             <span className="text-xl text-gray-600 tracking-tight font-normal dark:text-gray-50">
               <Link href="https://stelio.ai/">
                     <p className="hover:underline underline-offset-4">stelio</p>
@@ -54,7 +63,7 @@ export default function Home() {
                   </Link>
             </span>
           </div>
-          <div className="col-end-7 col-span-2 lg:px-20 pb-10 ">
+          <div className="col-end-7 col-span-2 lg:px-20 pb-7 ">
             <span className="text-xl text-gray-600 tracking-tight font-normal dark:text-gray-50">
             <Link href="https://aimazing.shop/">
                     <p className="hover:underline underline-offset-4">aimazing</p>
