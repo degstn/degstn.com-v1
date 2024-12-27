5dc53fbe5c38e07e3d03c7cd9144f02c3de24827
drew goldstein <drew@degstn.com>
2024-12-26 23:46:29 -0500

    added version viewer to site bc it looks cool

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -93,9 +93,37 @@ export default function Home() {
           </div>
         </div>
         <div className="grid grid-cols-10 lg:ml-10 lg:px-32 sm:px-0 gap-5 sm:mt-20 mb-16 ">
-          
+        <div className="col-end-3 lg:px-20 pt-1.5">
+  <Link
+    href="github.com/"
+    aria-label="Changelog"
+    className="inline-flex items-center space-x-1 whitespace-nowrap text-international-orange-engineering dark:text-international-orange hover:underline"
+  >
+    <svg
+      width={12}
+      height={12}
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M9.5 3.25a2.25 2.25 0 1 1 3 2.122V6A2.5 2.5 0 0 1 10 8.5H6a1 1 0 0 0-1 1v1.128a2.251 2.251 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.5 0v1.836A2.493 2.493 0 0 1 6 7h4a1 1 0 0 0 1-1v-.628A2.25 2.25 0 0 1 9.5 3.25Zm-6 0a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Zm8.25-.75a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM4.25 12a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Z"
+      />
+    </svg>
+    <span className="text-xs text-international-orange-engineering tracking-tight font-medium dark:text-international-orange">
+      prod
+    </span>
+    <span className="text-xs text-gray-600 tracking-tight font-medium dark:text-gray-50 pl-1">
+      v1.1.2
+    </span>
+  </Link>
+</div>
         <div className="col-end-7 col-span-2 lg:pl-20">
+          
   <div className='flex flex-row text-gray-600 dark:text-gray-50'>
+    
     <div className="px-5">
       <Link href="https://github.com/degstn/" aria-label="GitHub profile">
         <svg
