1d535dd998a39ba1cb60975cbb9205a8dda17008
drew goldstein <drew@degstn.com>
2025-01-05 21:53:30 -0500

    added metadata for photography page

--- frontend/src/app/photography/DotMatrixGlobe.tsx
+++ frontend/src/app/photography/DotMatrixGlobe.tsx
@@ -3,6 +3,37 @@
 import React, { useRef, useState, useEffect } from "react";
 import { polygon, bbox, booleanPointInPolygon } from "@turf/turf";
 import * as THREE from "three";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Photography - degstn.com",
+  description: "Shots from around the globe on degstn.com",
+  openGraph: {
+    title: "Photography - degstn.com",
+    description: "Shots from around the globe on degstn.com",
+    url: "https://degstn.com/photography",
+    siteName: "degstn.com",
+    images: [
+      {
+        url: "/photography-og.jpg", 
+        alt: "A globe representing worldwide photography",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  // icons: {
+  //   icon: "/favicon.ico",
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "Photography - degstn.com",
+  //   description: "Shots from around the globe on degstn.com",
+  //   images: ["/images/some-globe-icon.jpg"],
+  // },
+};
 
 /**
  * A single-file page that:
