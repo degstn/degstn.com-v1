3bfe58c02831e68e4d1076b9d7c9d6ccd089bd67
drew goldstein <drew@degstn.com>
2025-01-05 21:55:31 -0500

    put metadata in wrong spot, next only recognizes page.tsx's metadata

--- frontend/src/app/photography/page.tsx
+++ frontend/src/app/photography/page.tsx
@@ -1,6 +1,27 @@
 import React from "react";
 import DotMatrixGlobe from "./DotMatrixGlobe"; // The client component
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Photography - degstn.com",
+  description: "Shots from around the globe on degstn.com",
+  openGraph: {
+    title: "Photography - degstn.com",
+    description: "Shots from around the globe on degstn.com",
+    url: "https://degstn.com/photography",
+    siteName: "degstn.com",
+    images: [
+      {
+        url: "/photography-og.jpg", 
+        alt: "A globe representing worldwide photography",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function PhotographyPage() {
   return (
